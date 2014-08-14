b7d192382e7677870ff31334576ea75dc81cadf1
Harris Munir <hamunir@us.ibm.com>
2014-08-14 16:11:12 -0500

    completed rivets.js refactoring

--- static/js/main.js
+++ static/js/main.js
@@ -1,46 +1,84 @@
-var curResults; //Array of current search results
+var searchState = {
+	ready: false,
+	loading: false,
+	results: {}
+};
+
+
+rivets.bind($('#loadingPanel'), {
+	searchState: searchState
+});
+
+rivets.bind($('#resultsPanel'), {
+	searchState: searchState
+});
+
+rivets.bind($('#resultsTable'), {
+	searchState: searchState
+});
+
+rivets.formatters.size = function(value) {
+	if(value > 1024) {
+		return Math.round(value / 1024) + " MB";
+	} else {
+		return value + " KB";
+	}
+}
+
+rivets.formatters.date = function(value) {
+	return moment.utc(value).fromNow();
+}
 
 // Callback for when we recieve data from a search query request
-function renderSearchResults(data) {
-	curResults = data.results
+function processSearchResults(data) {
 	var now = moment.utc();
-	$('#resultsTable').find("tr").remove();
 	if(data.status !== "ok") {
 		console.log("Bad response from /search!")
 		console.log(data)
 	} else {
-		var index = 0;
-		data.results.forEach(function(entry) {
+		data.results.forEach(function(result) {
 			// Calculate days since last update
-			var lastUpdate = moment.utc(entry.last_update);
+			var lastUpdate = moment.utc(result.last_update);
 			var daysBetween = now.diff(lastUpdate, 'days');
 			// Calculate color coding classifications
-			entry.coding = {}
-			entry.coding.stars = starClassifier(entry.stars);
-			entry.coding.forks = forkClassifier(entry.forks);
-			entry.coding.language = langClassifier(entry.language);
-			entry.coding.size = sizeClassifier(entry.size_kb);
-			entry.coding.date = dateClassifier(daysBetween);
+			result.coding = {}
+			result.coding.stars = starClassifier(result.stars);
+			result.coding.forks = forkClassifier(result.forks);
+			result.coding.language = langClassifier(result.language);
+			result.coding.size = sizeClassifier(result.size_kb);
+			result.coding.date = dateClassifier(daysBetween);
+			//Add addToJenkins function
+			result.addToJenkins = function() {
+				addToJenkins(result);
+			};
+		});
 
-			$('#resultsTable').append(generateRow(entry, index));
-			rivets.bind($('#row-' + index), {
-				entry: entry
-			});
-			index++;
-		})
+		searchState.results = data.results
+		searchState.loading = false;
+		searchState.ready = true
 	}
 }
 
 // When the query textbox is changed, make a request to
 // /query
 $('#query').change(function(){
+	searchState.ready = false;
+	searchState.loading = true;
 	var query = $(this).val();
 	if(query.length > 0) {
-		$('#resultsTable').find("tr").remove();
-		$('#resultsTable').append("<tr><td>Loading...<td></tr>");
-		$.getJSON("/search", {q: query}, renderSearchResults);
+		$.getJSON("/search", {q: query}, processSearchResults);
 	}
 });
+
+function addToJenkins(repo) {
+	var requestData = {
+		name: repo.name,
+		github_url: repo.url,
+		git_url: repo.git_url,
+		default_branch: repo.default_branch
+	};
+	$.post("/createJob", requestData, addToJenkinsCallback, "json");
+}
 
 function addToJenkinsCallback(data) {
 	if(data.status === "ok") {
@@ -50,19 +88,6 @@ function addToJenkinsCallback(data) {
 		console.log(data);
 	}
 }
-
-// "Add to Jenkins" buttons
-$(document).on('click', '.add-to-jenkins', function() {
-	var index = $(this).attr("data-id");
-	var entry = curResults[index];
-	var requestData = {
-		name: entry.name,
-		github_url: entry.url,
-		git_url: entry.git_url,
-		default_branch: entry.default_branch
-	};
-	$.post("/createJob", requestData, addToJenkinsCallback, "json");
-})
 
 //debug
 $('#query').val("redis");
